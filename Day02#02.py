1526d202662d3802162de3c4fc10a4f0c875c94f
maggy888 <148534151+maggy888@users.noreply.github.com>
2023-10-21 19:05:15 +0800

    Create Day02#02.py

---
+++ Day02#02.py
@@ -0,0 +1,21 @@
+1. 輸入分別輸入兩個數字存到 x, y 變數
+
+2. 請將 x 和 y 變數加總後存入 sum 變數
+
+3. 在畫面上印出 sum 的結果
+
+❏ Sample Input： 123 456.789 ← 使用者輸入
+
+❏ Sample Output： 579.789 ← 畫面輸出
+
+❏ Sample Code：
+
+
+x = int(input("enter first number:"))
+
+y = float(input("enter second number:"))
+
+sum = float(x) + float(y)
+
+print(sum)
+
